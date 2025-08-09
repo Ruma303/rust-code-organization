3c10c34dd9dc027cacf5d1b69462ac9f277cd089
Ruma303 <matteoh8@hotmail.it>
2025-08-09 18:12:30 +0200

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,60 @@
-use rust_code_organization::product::{Product, category::Category};
-use rust_code_organization::customer::Customer;
-use rust_code_organization::order::{Order, order_status::OrderStatus};
+//% Simple modules
+mod instruments {
+    //, Public function
+    pub fn guitar() {
+        println!("Playing guitar");
+    }
+
+    //, Private function
+    fn piano() {
+        println!("Playing piano");
+    }
+
+    //, Getter
+    pub fn get_piano() {
+        self::piano();
+    }
+
+    //% Nested modules
+    pub mod electric {
+        pub fn guitar() {
+            println!("Playing electric guitar");
+        }
+    }
+}
 
 fn main() {
-    let p1 = Product::new(1, "Laptop".to_string(), 1000.0, Category::Electronics);
-    let p2 = Product::new(2, "Mouse".to_string(), 50.0, Category::Electronics);
+    instruments::guitar();
+    instruments::get_piano();
+    instruments::electric::guitar();
+    crate::instruments::guitar();
+}
 
-    let customer = Customer::new(1, "Mario Rossi".to_string(), "mario@example.com".to_string(), "123456789".to_string());
+//% Moudules accessibility
 
-    let order = Order::new(customer, vec![p1, p2], OrderStatus::Pending, 2, "Via Roma, 10".to_string(), "123456789".to_string());
+//* Generic modules
+pub mod public_module {
+    pub fn public_function() {}
+}
 
-    println!("Costo spedizione: {}", order.calculate_shipping_cost());
-    println!("Acquirente: {:?}", order.get_order_customer());
+//* Crate modules
+pub(crate) mod public_crate_module {
+    pub struct PublicStruct;
+    struct PrivateStruct;
+
+    //* Super modules
+    pub(super) mod public_super_module {
+        pub struct PublicStruct;
+        struct PrivateStruct;
+    }
+
+     pub mod child {
+
+      pub use crate::instruments::electric::guitar as electric_guitar;
+
+        pub(in crate::public_crate_module) fn restricted_function() {
+            println!("Accessible only in parent module");
+            electric_guitar();
+        }
+    }
 }
