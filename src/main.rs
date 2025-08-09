90f0e6e0bfea86d29adfcb93c8cb77542cdf3768
Ruma303 <matteoh8@hotmail.it>
2025-08-09 18:46:07 +0200

    ok

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,49 @@ fn main() {
     instruments::guitar();
     instruments::get_piano();
     instruments::electric::guitar();
-    crate::instruments::guitar();
+
+    crate::instruments::guitar(); // Uso di crate
+
+    use grandparent::parent::child::call_grandparent;
+    call_grandparent();
+
+    use my_module::function_b;
+    function_b();
 }
 
-//% Moudules accessibility
+//, super
+mod grandparent {
+    pub fn grandparent_function() {
+        println!("Called grandparent_function");
+    }
+
+    pub mod parent {
+        pub fn parent_function() {
+            println!("Called parent_function");
+        }
+
+        pub mod child {
+            use super::super::grandparent_function; // Risale di 2 livelli
+
+            pub fn call_grandparent() {
+                grandparent_function(); // Chiama direttamente grandparent_function
+            }
+        }
+    }
+}
+
+//, self
+mod my_module {
+    pub fn function_a() {
+        println!("Called function_a");
+    }
+
+    pub fn function_b() {
+        self::function_a(); // Usa self per accedere a function_a
+    }
+}
+
+//% Modules accessibility
 
 //* Generic modules
 pub mod public_module {
@@ -48,9 +87,9 @@ pub(crate) mod public_crate_module {
         struct PrivateStruct;
     }
 
-     pub mod child {
+    pub mod child {
 
-      pub use crate::instruments::electric::guitar as electric_guitar;
+        pub use crate::instruments::electric::guitar as electric_guitar;
 
         pub(in crate::public_crate_module) fn restricted_function() {
             println!("Accessible only in parent module");
