7deacf56325300d941f1eb7b3b8a89f255094f62
Ruma303 <matteoh8@hotmail.it>
2025-10-25 10:34:38 +0200

    moduli inline

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+//% Simple modules
+mod instruments {
+    //# Public function
+    pub fn guitar() {
+        println!("Playing guitar");
+    }
+
+    //# Private function
+    fn piano() {
+        println!("Playing piano");
+    }
+
+    //# Getter
+    pub fn get_piano() {
+        self::piano();
+    }
+
+    //% Nested modules
+    pub mod electric {
+        pub fn guitar() {
+            println!("Playing electric guitar");
+        }
+    }
+}
+
+fn main() {
+    instruments::guitar();
+    instruments::get_piano();
+    instruments::electric::guitar();
+}
